32c7c57b56387a14e1a714d550e81b8abfb50d35
eapmon <pintoeric16@gmail.com>
2025-05-08 21:16:50 -0500

    update installation script to include build-essential and python3-dev dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y libzbar-dev libzbar0 zbar-tools
+sudo apt-get update && sudo apt-get install -y \
+    libzbar-dev \
+    libzbar0 \
+    zbar-tools \
+    build-essential \
+    python3-dev
 pip install zbar
