afe215a347321cf6041f86a4e7541f48d17f9853
eapmon <pintoeric16@gmail.com>
2025-05-08 21:39:41 -0500

    update installation script to install pyzbar instead of zbar

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y \
-    libzbar-dev \
-    libzbar0 \
-    zbar-tools \
-    build-essential \
-    python3-dev \
-    gcc
-pip install zbar
+    sudo apt-get update && sudo apt-get install -y \
+        libzbar-dev \
+        libzbar0 \
+        zbar-tools \
+        build-essential \
+        python3-dev \
+        gcc
+
+    pip install pyzbar
