47be976a2aef57295206d4ec78ddeb2129a40e61
eapmon <pintoeric16@gmail.com>
2025-05-08 21:08:01 -0500

    update installation script to include python-zbar dependency

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y libzbar-dev libzbar0 zbar-tools
+sudo apt-get update && sudo apt-get install -y libzbar-dev libzbar0 zbar-tools python-zbar
